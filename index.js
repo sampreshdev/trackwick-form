0ddc9052944a5e6ae4f604d9bb19053e9d1f7f72
sampreshdev <153799622+sampreshdev@users.noreply.github.com>
2024-09-13 18:51:26 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+      let element = document.createElement('script');
+      element.src = 'https://publicformdev.trackwick.com/app.js';
+      element.defer = 'defer';
+      document.head.appendChild(element);
+});
