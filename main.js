cb8f5bc91714c6b3c68726c0141b45fb159b07e3
sampreshdev <153799622+sampreshdev@users.noreply.github.com>
2024-09-13 17:18:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function loadForm(load) {
 	if (load) {
 		let element = document.createElement('script');
-		element.src = 'https://publicformdev.trackwick.com/main.js';
+		element.src = 'https://publicformdev.trackwick.com/app.js';
 		element.defer = 'defer';
 		document.head.appendChild(element);
 	}
